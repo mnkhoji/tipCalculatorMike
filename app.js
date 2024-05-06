9f21e2aaaa441f32aa2fc3f148a9fe5eaa2660c6
mnkhoji <161701934+mnkhoji@users.noreply.github.com>
2024-05-06 12:18:46 +0000

    changes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const buttonEL = document.querySelector("calcbutton");
-const BillAmount = document.querySelector("billAmount");
-const TipAmount = document.querySelector("tip");
-const TotalAmount = document.querySelector("total");
+const buttonEl = document.querySelector("#calcbutton");
+const BillAmount = document.querySelector("#billAmount");
+const TipAmount = document.querySelector("#tip");
+const TotalAmount = document.querySelector("#total");
 
 
 function totalCalculator() {
     const BillValue = BillAmount.value;
     const TipValue = TipAmount.value;
-    const totalValue = BillValue + (BillValue * TipValue)/100;
+    const totalValue = BillValue * (1 + TipValue / 100)
+    //BillValue + (BillValue*TipValue)/100;
     TotalAmount.innerText = totalValue.toFixed(2);
 }
 
-buttonEL.addEventListener("click", totalCalculator);
+buttonEl.addEventListener("click", totalCalculator);
+
